42018cbc1d86978dfa78acc0413d3bf8c15428db
Yun Suk Chang <yunsukchang@gmail.com>
2016-10-06 22:21:13 -0700

    Mouse input manager added. Left click to move the camera. Right click to rotate camera. No click mouse movement to move the tool. Scroll for z directional movement.

---
+++ vSculpt/Assets/Scripts/MouseInputManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseInputManager : MonoBehaviour {
+
+	public float toolSpeedX = 0.01f;
+	public float toolSpeedY = 0.01f;
+	public float toolSpeedZ = 1;
+
+	public float cameraMoveSpeedX = 0.1f;
+	public float cameraMoveSpeedY = 0.1f;
+	public float cameraMoveSpeedZ = 1;
+
+	public float cameraRotationSpeedX = 2;
+	public float cameraRotationSpeedY = 2;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mouseXMovement = Input.GetAxis ("Mouse X");
+		float mouseYMovement = Input.GetAxis ("Mouse Y");
+		float scrollMovement = Input.GetAxis ("Mouse ScrollWheel");
+
+		if (Input.GetMouseButton (0) || Input.GetMouseButton (2) || Input.GetKey(KeyCode.LeftShift)) {
+			Camera.main.transform.position += Camera.main.transform.rotation *
+				new Vector3(cameraMoveSpeedX * mouseXMovement,cameraMoveSpeedX * mouseYMovement, cameraMoveSpeedX * scrollMovement);
+			return;
+		}
+		if (Input.GetMouseButton (1)) {
+			//Quaternion rotX = Quaternion.AngleAxis (cameraRotationSpeedX * mouseXMovement, Vector3.up);
+			//Quaternion rotY = Quaternion.AngleAxis (cameraRotationSpeedX * mouseYMovement, Camera.main.transform.right);
+			Vector3 angles = Camera.main.transform.localEulerAngles;
+			angles.x += cameraRotationSpeedY * mouseYMovement;
+			angles.y += cameraRotationSpeedX * mouseXMovement;
+			Camera.main.transform.localEulerAngles = angles; //= .y = //.rotation *= rotX * rotY;
+			print(Camera.main.transform.up);
+			return;
+		}
+
+
+		transform.position += Camera.main.transform.rotation *
+			new Vector3(toolSpeedX * mouseXMovement, toolSpeedY * mouseYMovement, toolSpeedZ * scrollMovement);
+
+	}
+}
