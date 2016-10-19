528ec581c80d738458b557c677883eb8428489bc
Yun Suk Chang <yunsukchang@gmail.com>
2016-10-19 13:31:06 -0700

    Made camera move direction natural

--- vSculpt/Assets/Scripts/MouseInputManager.cs
+++ vSculpt/Assets/Scripts/MouseInputManager.cs
@@ -28,7 +28,7 @@ public class MouseInputManager : MonoBehaviour {
 
 		if (Input.GetMouseButton (0) || Input.GetMouseButton (2) || Input.GetKey(KeyCode.LeftShift)) {
 			Camera.main.transform.position += Camera.main.transform.rotation *
-				new Vector3(cameraMoveSpeedX * mouseXMovement,cameraMoveSpeedX * mouseYMovement, cameraMoveSpeedX * scrollMovement);
+				new Vector3(-cameraMoveSpeedX * mouseXMovement, -cameraMoveSpeedY * mouseYMovement, cameraMoveSpeedZ * scrollMovement);
 			return;
 		}
 		if (Input.GetMouseButton (1)) {
