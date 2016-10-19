a3808c02e0cc229da66afe1e1cbb0214b298b327
Yun Suk Chang <yunsukchang@gmail.com>
2016-10-19 13:31:52 -0700

    Made Start Menu

---
+++ vSculpt/Assets/StartScreen.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreen : MonoBehaviour {
+
+	public void OnStartButtonClicked()
+	{
+		transform.gameObject.SetActive (false);
+	}
+
+	public void OnExitButtonClicked()
+	{
+		Application.Quit ();
+	}
+}
