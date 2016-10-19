b939ed8f526db167847ba1145c221417edcd483e
Yun Suk Chang <yunsukchang@gmail.com>
2016-10-19 14:36:22 -0700

    Added pause screen UI and connected its component with MainSceneUI's Stop Recording button

---
+++ vSculpt/Assets/Scripts/PauseScreen.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Deactivate the Pause Screen Components
+		transform.GetChild (0).gameObject.SetActive (false);
+	}
+		
+	public void OnStopRecordingButtonClicked()
+	{
+		transform.GetChild (0).gameObject.SetActive (true);
+		print ("Stop Recording Button Clicked!");
+	}
+
+	public void OnResumeButtonClicked()
+	{
+		transform.GetChild (0).gameObject.SetActive (false);
+	}
+
+	public void OnExportModelButtonClicked()
+	{
+		// TODO: Do Export Model
+	}
+
+	public void OnExitButtonClicked()
+	{
+		Application.Quit ();
+	}
+}
