42e6e19f8f18505fabb37bf16650dd0906d774fd
Yun Suk Chang <yunsukchang@gmail.com>
2016-11-02 15:02:44 -0700

    Made tool turnoff rendering of tetrahedron elements of cubes when they are interacted with the tool

---
+++ vSculpt/Assets/Scripts/MeshDestroyer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshDestroyer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		foreach (Transform child in transform) {
+			GameObject childGO = child.gameObject;
+			if (childGO.GetComponent<MeshCollider> () == null) {
+				MeshCollider mc = childGO.AddComponent<MeshCollider> ();
+				mc.sharedMesh = childGO.GetComponent<MeshFilter> ().sharedMesh;
+				mc.convex = true;
+				mc.isTrigger = true;
+
+				childGO.AddComponent<TriggerEvent> ();
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ vSculpt/Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerEvent : MonoBehaviour {
+	void OnTriggerEnter(Collider other)
+	{
+		transform.gameObject.GetComponent<MeshRenderer> ().enabled = false;
+	}
+}
