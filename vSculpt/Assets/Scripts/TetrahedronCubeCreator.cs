52e2d6c0d9c44077ac683c62c1f9f31c5881c7d6
Yun Suk Chang <yunsukchang@gmail.com>
2016-11-02 16:08:41 -0700

    Tried out using more tetrahedron cubes using Tetrahedron Cube Creator. It looks better, but it is too slow to render. Find better rendering approach

---
+++ vSculpt/Assets/Scripts/TetrahedronCubeCreator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TetrahedronCubeCreator : MonoBehaviour {
+
+	public GameObject Cube;
+	public float CubeSideLength = 0.01f;
+	public int CubePerSide = 10;
+
+	// Use this for initialization
+	void Start () {
+
+		float startX = CubeSideLength - CubeSideLength * CubePerSide/2;
+		float startY = 0.525f;
+		float startZ = 0.45f;
+		for (int i = 0; i < CubePerSide; i++) {
+			for (int j = 0; j < CubePerSide; j++) {
+				for (int k = 0; k < CubePerSide; k++) {
+					Instantiate (Cube, new Vector3 (startX + CubeSideLength * i, startY + CubeSideLength * j, startZ + CubeSideLength * k), new Quaternion ());
+				}
+			}
+		}
+	}
+	
+
+}
